ed83c13bfb2c2cf592d72d3bac0cb6c2d1570d87
Dawid <trystek@gmail.com>
2015-05-27 14:04:15 +0200

    Added logging with GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
   # :recoverable, :rememberable and :trackable
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :omniauthable, omniauth_providers: [:github]
   acts_as_voter
   mount_uploader :avatar, AvatarUploader
 
@@ -20,6 +20,15 @@ class User < ActiveRecord::Base
   def add_points(value)
     self.points += value
     self.save
+  end
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.nickname
+      user.remote_avatar_url = auth.info.image
+    end
   end
   
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526150623) do
+ActiveRecord::Schema.define(version: 20150527105709) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20150526150623) do
     t.integer  "points"
     t.string   "name"
     t.string   "avatar"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
